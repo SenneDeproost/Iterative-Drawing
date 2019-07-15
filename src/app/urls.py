07023bd737600ea74eeaf7d9052236b299c1133e
Senne Deproost <sennedeproost@hotmail.com>
2019-07-15 10:36:27 +0200

    Added index to views

--- src/app/urls.py
+++ src/app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('index.urls')),
     path('form/', include('form.urls')),
     path('admin/', admin.site.urls),
-    path('', include('index.url'))
 ]
