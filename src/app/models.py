85a0577877e2d840b4ca21854f1bb2ea247d507f
Senne Deproost <sennedeproost@hotmail.com>
2019-07-15 15:00:49 +0200

    Going to use built in sessions

--- src/app/models.py
+++ src/app/models.py
@@ -4,12 +4,21 @@ import time
 A Session is created after the registration of a user. Both Training and Testing classes are used to draw upon a 
 set of examples the user can be trained and tested on.
 """
+
+class Participant:
+    def __init__(self, form):
+        self.first_name = form.cleaned_data.get('first_name')
+        self.last_name = form.cleaned_data.get('last_name')
+        self.age = form.cleaned_data.get('age')
+        self.email = form.cleaned_data.get('email')
+
+
 class Session:
     # Session is initialized with the information obtained by the participant's form.
     def __init__(self, form):
-        self.timestamp = time.time()        # Timestamp to record session time
-        self.participant = form             # Participant details
-        self.status = 'registered'          # Record status of the participant (registered, trained or tested)
+        self.timestamp = time.time()             # Timestamp to record session time
+        self.participant = Participant(form)     # Participant details
+        self.status = 'registered'               # Record status of the participant (registered, trained or tested)
 
         self.training = self.create_training()
         self.testing = self.create_testing()
@@ -22,12 +31,3 @@ class Session:
 
     def save(self):
         print("Saving session into database")
-
-
-class Training:
-    def __init__(self):
-        self.set = []
-
-class Testing:
-    def __init__(self):
-        self.set = []
--- src/form/views.py
+++ src/form/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
-from .form import RegistrationForm
+from .models import RegistrationForm
+import time
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+#from app.models import Session
 
 
 def form(request):
-    # if this is a POST request we need to process the form data
+    # Get data out form when POST method is used
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+        # Get the form from the POST request
         form = RegistrationForm(request.POST)
-        # check whether it's valid:
+        # If the from is valid, extract the data from the form into a Session
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect(request, "training.html")
+            # Add user details to the current session
+            request.session['first_name'] = form.first_name
+            request.session['last_name'] = form.last_name
+            request.session['age'] = form.age
+            request.session['email'] = form.email
+            request.session['timestamp'] = time.time()
 
-    # if a GET (or any other method) we'll create a blank form
+            # Redirect to the next page after registration
+            return HttpResponseRedirect('/training')
     else:
         form = RegistrationForm()
 
