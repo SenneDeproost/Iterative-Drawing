ff2530cfb8db5c85b16f8bf195d3456ab9ffb543
Senne Deproost <sennedeproost@hotmail.com>
2019-07-16 12:02:56 +0200

    Succesfull migration from TrainingSession to DB

--- src/app/settings.py
+++ src/app/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'training',
+    'testing',
+    'form',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/form/models.py
+++ src/form/models.py
@@ -3,6 +3,7 @@ from django import forms
 
 # Registration form for new users
 class RegistrationForm(forms.Form):
+
     first_name = forms.CharField(label='First name', max_length=100)
     last_name = forms.CharField(label='Last name', max_length=100)
     age = forms.IntegerField(label='Age', min_value=0, max_value=150)
