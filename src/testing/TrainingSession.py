78f38c1ef296ae7410621c2c598ab8aadc8b65d6
Senne Deproost <senne@Frank.local>
2019-07-17 17:17:53 +0200

    Added load_cases method to TrainingSession

--- src/testing/TrainingSession.py
+++ src/testing/TrainingSession.py
@@ -1,18 +1,51 @@
 import csv
 import json
 import math
+import random
+from os import listdir
 
+# A training session consists of several training cases. These cases are randomly drawn from the case directory or can
+# specified as a list of case files.
 class TrainingSession:
     def __init__(self, participant):
         self.participant = participant
         self.tolerance = 0.25            # Default error tolerance
         self.numberOfCases = 5           # Default number of test cases
-        self.caseDir = "/draw_cases"     # Directory for training cases
+        self.caseDir = "./draw_cases"     # Directory for training cases
         self.results = []                # Results of the training session
+        self.cases = []                  # Initialize with no cases loaded
+
+    # Load TrainingCases
+    def load_cases(self, *args):
+        cases = []
+        if args:
+            # If a list od cases is given, load them into the session
+            for arg in args:
+                cases.append(TrainingCase(arg, self.tolerance).load_case())
+        else:
+            # Load random cases
+            for file in random.shuffle(listdir(self.caseDir)):
+                if file.endswith(".csv"):
+                    cases.append(TrainingCase(file, self.tolerance).load_case())
+                if len(cases) == self.numberOfCases:
+                    break
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class TrainingCase
-    def __init__(self, tolerance):
-        self.casePath = None            # Which case
+    def __init__(self, case_path, tolerance):
+        self.casePath = case_path       # Which case
         self.numberOfTrials = 0         # How many times submitted
         self.numberOfErrors = 0         # How many times wrong, according to tolerance
         self.error = None               # What was the error in the last submission
@@ -27,8 +60,8 @@ class TrainingCase
 
 
 class TrainingTrial:
-    def __init__(self, caseData, tolerance):
-        self.caseData = caseData
+    def __init__(self, case_data, tolerance):
+        self.caseData = case_data
         self.tolerance = tolerance
         self.error = None
 
@@ -50,6 +83,9 @@ class TrainingTrial:
                 if lowest_distance > distance:
                     lowest_distance = distance
             distances.append(lowest_distance)
+
+
+
 
 
 
