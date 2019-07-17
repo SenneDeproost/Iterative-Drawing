82076bdf834c4f76bf297982a0bb69c717940609
Senne Deproost <sennedeproost@hotmail.com>
2019-07-17 14:33:08 +0200

    Added TrainingTrial

--- src/testing/TrainingSession.py
+++ src/testing/TrainingSession.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import math
 
 class TrainingSession:
     def __init__(self, participant):
@@ -23,6 +24,37 @@ class TrainingCase
         # Map the CSV file onto JSON
         reader = csv.DictReader(file, fieldnames=("x", "y"))
         return json.dumps([row for row in reader])
+
+
+class TrainingTrial:
+    def __init__(self, caseData, tolerance):
+        self.caseData = caseData
+        self.tolerance = tolerance
+        self.error = None
+
+    # Check user input with the path of the case. The input is an JSON array of X Y key fields describing the
+    # coordinates.
+    def check(self, user_input):
+        distances = []
+        # Load JSON into list
+        user_input = json.loads(user_input)
+        # Check distance error for every point in case path
+        for casePoint in self.caseData:
+            case_x = casePoint['x']
+            case_y = casePoint['y']
+            lowest_distance = None
+            for inputPoint in user_input:
+                input_x = inputPoint['x']
+                input_y = inputPoint['y']
+                distance = math.sqrt((case_x - input_x)**2 + (case_y - input_y)**2)
+                if lowest_distance > distance:
+                    lowest_distance = distance
+            distances.append(lowest_distance)
+
+
+
+
+
 
 
 
