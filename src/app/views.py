8c9dd950705bdca4496792c311e47f30c01a70b0
Senne Deproost <sennedeproost@hotmail.com>
2019-07-31 18:21:33 +0200

    preventDefault fix

--- src/app/views.py
+++ src/app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+# This should be included: from Experiment import experiment
 from Experiment import experiment
 
 
--- src/statics/js/draw.js
+++ src/statics/js/draw.js
@@ -49,17 +49,17 @@ window.onload = function () {
     // Prevent scrolling when touching the canvas
     document.body.addEventListener("touchstart", function (e) {
         if (e.target == canvas) {
-         //   e.preventDefault();
+            e.preventDefault();
         }
     }, false);
     document.body.addEventListener("touchend", function (e) {
         if (e.target == canvas) {
-           // e.preventDefault();
+            e.preventDefault();
         }
     }, false);
     document.body.addEventListener("touchmove", function (e) {
         if (e.target == canvas) {
-          //  e.preventDefault();
+            e.preventDefault();
         }
     }, false);
 
