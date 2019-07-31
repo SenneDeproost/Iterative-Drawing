ca398c8182559c45b205d464500f7e920cd31777
Senne Deproost <sennedeproost@hotmail.com>
2019-07-31 20:58:11 +0200

    Deleted preventDefault in draw.js

--- src/statics/js/draw.js
+++ src/statics/js/draw.js
@@ -45,25 +45,6 @@ window.onload = function () {
         canvas.removeEventListener('mousemove', onPaint, false);
     }, false);
 
-
-    // Prevent scrolling when touching the canvas
-    document.body.addEventListener("touchstart", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
-    document.body.addEventListener("touchend", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
-    document.body.addEventListener("touchmove", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-        }
-    }, false);
-
-
     // Equivalence for touch devices
     // Set up touch events for mobile, etc
     canvas.addEventListener("touchstart", function (e) {
