3d593c0acca71f62916740bfb2667c41fb68794e
Senne Deproost <sennedeproost@hotmail.com>
2019-07-31 18:18:48 +0200

    Fixed experiment unknown bug

--- src/app/views.py
+++ src/app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+from Experiment import experiment
 
 
 def home(request):
