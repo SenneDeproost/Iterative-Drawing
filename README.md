82233760c7c5051deac25d0a8bcaade68af0d808
Senne Deproost <sennedeproost@hotmail.com>
2019-08-02 11:26:52 +0200

    Added comments to app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# follow_the_leader
-Web application for Professor De Boer @ VUB AI Lab, concerning language evolution.
+# Iterated Drawing Application 
+
+Web application for Professor De Boer @ VUB AI Lab, concerning language evolution. The Django framework is used, following a model-view-controller model. Verification and storage of data is handled by the backend part of the project.
+
+The source code of the project is contained within the *src* folder.
+
+## Setup
+
+In the *src* folder, the *activate_experiment.sh* file can be executed to start the Django server. 
+
+## Frontend
+
+## Backend
+
--- src/app/views.py
+++ src/app/views.py
@@ -3,6 +3,7 @@ import sys
 
 from django.shortcuts import render
 
+# Prevent error for relative import
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
@@ -10,12 +11,15 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from Experiment import experiment
 
 
+# Render home page
 def home(request):
     return render(request, "home.html")
 
-
+# Render thank you page
 def thanks(request):
+    # experiment variable is globally defined in the Experiments.py class.
     global experiment
+    # When the thank you page is loaded, the experiment is saved and reset for a next session.
     experiment.save(request.session)
     experiment.reset()
     return render(request, "thanks.html")
